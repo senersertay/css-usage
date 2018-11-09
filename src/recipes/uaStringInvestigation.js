130c634fb38c669367a68333efa885f171b089a7
Sertay Sener <sesener@microsoft.com>
2018-11-08 16:41:26 -0800

    Spacing fixed.

--- src/recipes/uaStringInvestigation.js
+++ src/recipes/uaStringInvestigation.js
@@ -18,18 +18,18 @@ void function() {
             const keywords = ['edge', 'chrome', 'browser', 'unsupported', 'ie', 'internet explorer', 'google'];
             const patterns = [/edge/gi, /chrome/gi, /browser/gi, /unsupported/gi, /ie/gi, /internet ?explorer/gi, /google/gi];
 
-        for(var i=0; i<patterns.length ;i++) {
-            var found = document.body.textContent.match(patterns[i]);
+            for(var i=0; i<patterns.length ;i++) {
+                var found = document.body.textContent.match(patterns[i]);
 
-            if(found) {
-                results[keywords[i]] = { hits: found.length };
-            } else {
-                results[keywords[i]] = { hits: 0 };
+                if(found) {
+                    results[keywords[i]] = { hits: found.length };
+                } else {
+                    results[keywords[i]] = { hits: 0 };
+                }
             }
-        }
 
-        hasLogged = true;
-        return results;
+            hasLogged = true;
+            return results;
         }
         
     });
